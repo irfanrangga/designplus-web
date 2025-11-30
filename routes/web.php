47dc083cbd9a25dfcc5166f40b523bd6c5cb980c
hannndii <shiirogami95@gmail.com>
2025-11-30 15:52:52 +0700

    migrasi produk-page, buat database products, mindahin asset lama ke project baru

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+// 1. Rute Beranda (Root URL) -> Arahkan ke view homepage
+Route::get('/', function () {
+    return view('homepage'); // Pastikan file resources/views/homepage.blade.php ada
+})->name('home');
 
-// Route::get('/test', function () {
-//     return 'This is a test route.';
-// });
+// 2. Rute Layanan -> Arahkan ke view layanan
+Route::get('/layanan', function () {
+    return view('layanan'); // Pastikan file resources/views/layanan.blade.php ada
+})->name('layanan');
 
-Route::get('/product', function () {
-    return view('product-page');
-});
+// 3. Rute Produk (Menggunakan Controller yang sudah kita buat)
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');

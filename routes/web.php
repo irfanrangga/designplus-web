6d9332ccebe45713a3609cd21eb909a7c904cbe0
hannndii <shiirogami95@gmail.com>
2025-11-30 15:59:47 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
-// 1. Rute Beranda (Root URL) -> Arahkan ke view homepage
 Route::get('/', function () {
-    return view('homepage'); // Pastikan file resources/views/homepage.blade.php ada
+    return view('homepage'); 
 })->name('home');
 
-// 2. Rute Layanan -> Arahkan ke view layanan
 Route::get('/layanan', function () {
-    return view('layanan'); // Pastikan file resources/views/layanan.blade.php ada
+    return view('layanan'); 
 })->name('layanan');
 
-// 3. Rute Produk (Menggunakan Controller yang sudah kita buat)
 Route::get('/product', [ProductController::class, 'index'])->name('product.index');
 Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');

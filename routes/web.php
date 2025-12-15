f3a96da55e78b1e867d3a4ac6aa8ea02d482a039
DonzKrieg <iromdhon2004@gmail.com>
2025-12-15 10:43:28 +0700

    tes

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('homepage'); 
+})->name('home');
 
-Route::get('/test', function () {
-    return 'This is a test route.';
-});
+Route::get('/layanan', function () {
+    return view('layanan'); 
+})->name('layanan');
 
-Route::get('/product', function () {
-    return view('product-page');
-});
+Route::get('/product', [ProductController::class, 'index'])->name('product.index');
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');

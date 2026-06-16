e5f633b04f53c0d9b94f631446566c1052c4ffdf
Irfan Rangga <150642564+irfanrangga@users.noreply.github.com>
2026-06-16 20:27:03 +0700

    Add cache-busting query string to product.js

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
         rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
-    <script src="{{ asset('js/product.js') }}"></script>
+    <script src="{{ asset('js/product.js?v=' . time()) }}"></script>
     <title>Detail Produk</title>
 </head>
 
